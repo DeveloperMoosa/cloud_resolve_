025912b1b5bac0bee18e7642e75d278218a444c6
Moosa <itxmoosaofficial@gmail.com>
2025-08-27 11:05:38 -0700

    Save local changes before pull

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: '/cloud_resolve/', // 👈 add this line
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -26,7 +25,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "docs"), // 👈 updated
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
   server: {
